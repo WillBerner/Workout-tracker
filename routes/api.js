da66c0a707dc575db941f613e4b08e084c10530b
Will Berner <will.o.berner@gmail.com>
2021-10-13 13:58:29 -0400

    Started on routing file. Started on workout model schema.

---
+++ routes/api.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+router.get("/api/workouts", ({body}, res) => {
+
+});
+
+router.put("/api/workouts/:id", ({body}, res) => {
+
+});
+
+router.post("/api/workouts", ({body}, res) => {
+
+});
+
+router.get("/api/workouts/range", ({body}, res) => {
+
+});
---
+++ server.js
@@ -0,0 +1,29 @@
+// Import dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Set local port to 3000 if not deployed
+const PORT = process.env.PORT || 3000;
+
+// Create express app
+const app = express();
+
+// Boilerplate express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static webpages
+app.use(express.static("public"));
+
+// Connect to Mongo
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+// Connect routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running at https://localhost:${PORT}`);
+});
